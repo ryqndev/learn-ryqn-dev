7bcb8cca3401a19c8da6da7d844043943a7b2e85
Ryan Yang <ryanqyang@gmail.com>
2023-11-02 21:08:50 -0700

    Added onclick

--- src/content/tutorial/webapp-demos/zothacks-web-design-2023/docs/script.js
+++ src/content/tutorial/webapp-demos/zothacks-web-design-2023/docs/script.js
@@ -4,7 +4,7 @@ function getRecipe() {
     let ingredients = document.getElementById("ingredients").value;
 
     fetch(
-        `https://api.spoonacular.com/recipes/complexSearch?apiKey=${SPOONACULAR_API_KEY}&ingredients=${ingredients}&number=1&addRecipeInformation=true`
+        `https://api.spoonacular.com/recipes/complexSearch?apiKey=${SPOONACULAR_API_KEY}&ingredients=${ingredients}&number=10&addRecipeInformation=true`
     )
         .then((res) => res.json())
         .then(renderRecipe);
@@ -18,6 +18,44 @@ function renderRecipe(recipeData) {
         return;
     }
 
-    let recipe = recipeData.results[0];
-    document.getElementById("results").innerHTML = JSON.stringify(recipe);
+    // clear existing html in results sections
+    document.getElementById("results").innerHTML = "";
+
+    for (let idx = 0; idx < recipeData.results.length; idx++) {
+        let recipe = recipeData.results[idx];
+
+        document.getElementById("results").innerHTML += `
+            <div class="result" id="result-${idx}">
+                <h2>
+                ${recipe.title}
+                </h2>
+                ${recipe.readyInMinutes} mins
+                <br />
+                ${recipe.diets}
+            </div>
+        `;
+    }
+    // Why not put into 1 for loop? because the way addEventListener works with .innterHTML
+    // i need to generate all the HTML first, and then attach the onclicks so that when I += the innterHTML, it
+    // won't rewrite it and remove the listeners
+    for (let idx = 0; idx < recipeData.results.length; idx++) {
+        let recipe = recipeData.results[idx];
+
+        document
+            .getElementById("result-" + idx)
+            .addEventListener("click", function () {
+                showRecipe(recipe);
+            });
+    }
+}
+
+function showRecipe(recipe) {
+    console.log(recipe);
+    let popup = new Popup({
+        title: recipe.title,
+        content: `
+            An example popup.
+            Supports multiple lines.${recipe}`,
+        showImmediately: true,
+    });
 }
